09fd6c7c507e527d5fbbcb42a16c25738c4aac33
Alan Semenov <ase@enonic.com>
2021-08-09 19:08:12 +0200

    Switched to lib-static for serving assets. Switched to HtmlWebpackPlugin for auto injection of built assets. Switched from Thymeleaf to Mustache for simplified template rendering (#148)

--- src/main/resources/webapp/webapp.ts
+++ src/main/resources/webapp/webapp.ts
@@ -1,9 +1,12 @@
-const thymeleaf = __non_webpack_require__('/lib/thymeleaf');
-const view = resolve('../template/main.html');
+const mustache = __non_webpack_require__('/lib/mustache');
+const portalLib = __non_webpack_require__('/lib/xp/portal');
+const view = resolve('../assets/index.html');
 
 export function get() {
     log.info('Hello from transpiled Typescript server-side code');
     return {
-        body: thymeleaf.render(view, {})
+        body: mustache.render(view, {
+            staticAssetUrl: portalLib.serviceUrl({service: 'staticAsset'})
+        })
     }
 }
